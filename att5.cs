4d6618d85796bf68d8885a381b84249ab6f664ab
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-03-01 20:02:16 -0300

    Add files via upload

---
+++ att5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int votostotal, votosBrancos, votosnulos, votosValidos;
+
+
+            Console.WriteLine("Total de votos");
+            votostotal = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Votos Brancos");
+            votosBrancos  = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Votos nulos");
+             votosnulos = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Votos VAlidos");
+            votosValidos = int.Parse(Console.ReadLine());
+
+            double porcentagemb = (100 * votosBrancos) / votostotal;
+            double porcentagmn = (100 * votosnulos) / votostotal;
+            double porcentagemv = (100 * votosValidos) / votostotal;
+
+
+            Console.WriteLine( "Porcentagem votos Brancos:" + porcentagemb);
+            Console.WriteLine("Porcentagem votos Nulos:" + porcentagmn);
+            Console.WriteLine("Porcentagem votos Validos:" + porcentagemv);
+            Console.ReadLine();
+             
+
+        }
+    }
+}
