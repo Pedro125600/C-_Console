b9b60b420e0922781e39b22be0aa6290597870e8
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-03-01 20:11:06 -0300

    Add files via upload

---
+++ att6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double varA, varB , varc;
+
+            Console.WriteLine("Valor de A");
+            varA = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor de B");
+            varB = double.Parse(Console.ReadLine());
+
+
+            varc = varA;
+            varA = varB;
+            varB = varc;
+
+            Console.WriteLine("Variavel A:" + varA);
+            Console.WriteLine("Variavel B:" + varB);
+            Console.ReadLine();
+        }
+    }
+}
