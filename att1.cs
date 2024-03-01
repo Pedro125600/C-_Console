06f579fd69a12b1a42bb2278c404d734421453d6
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-03-01 19:55:57 -0300

    Add files via upload

---
+++ att1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o lado do quadrado:");
+            double lado = double.Parse(Console.ReadLine());
+
+            double perimetro = 4 * lado;
+            double area = lado * lado;
+            double diagonal = Math.Sqrt(2) * lado;
+
+            Console.WriteLine("Perímetro do quadrado:" + perimetro);
+            Console.WriteLine("Área do quadrado:" + area);
+            Console.WriteLine("Diagonal do quadrado:" + diagonal);
+
+            Console.ReadLine();
+        }
+    }
+}
